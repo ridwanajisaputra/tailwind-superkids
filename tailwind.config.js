da1a426acf937728cf2d188f532bf9662e51b714
ridwanajisaputra <jendralfps@gmail.com>
2025-12-14 21:01:00 +0700

    font

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./**/*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        catamaran: ['Catamaran', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
